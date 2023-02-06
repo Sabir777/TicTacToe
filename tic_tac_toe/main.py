49caa54421ff2eda83d2d8561cf5c2fe5125a838
Sabir777 <sabir.yanin2014@yandex.ru>
2023-02-06 21:04:24 +0300

    Ввожу приватные члены класса. Изменяю объекты-свойства

--- tic_tac_toe/main.py
+++ tic_tac_toe/main.py
@@ -14,6 +14,11 @@ class TicTacToe:
     HUMAN_X = 1  # крестик (игрок - человек)
     COMPUTER_O = 2  # нолик (игрок - компьютер)
 
+    def __init__(self):
+        self.all_cell = None
+        self.pole = None
+        self.init()
+
     def init(self):
         self.all_cell = [(i, j) for i in range(3) for j in range(3)]
         self.pole = tuple(tuple(Cell() for _ in range(3)) for _ in range(3))
@@ -96,33 +101,45 @@ class TicTacToe:
         return self.is_draw and bool(self.all_cell)
 
 
+cell = Cell()
+assert cell.value == 0, "начальное значение атрибута value объекта класса Cell должно быть равно 0"
+assert bool(cell), "функция bool для объекта класса Cell вернула неверное значение"
+cell.value = 1
+assert bool(cell) == False, "функция bool для объекта класса Cell вернула неверное значение"
+
+assert hasattr(TicTacToe, 'show') and hasattr(TicTacToe, 'human_go') and hasattr(TicTacToe, 'computer_go'), "класс TicTacToe должен иметь методы show, human_go, computer_go"
+
 game = TicTacToe()
+assert bool(game), "функция bool вернула неверное значения для объекта класса TicTacToe"
+assert game[0, 0] == 0 and game[2, 2] == 0, "неверные значения ячеек, взятые по индексам"
+game[1, 1] = TicTacToe.HUMAN_X
+assert game[1, 1] == TicTacToe.HUMAN_X, "неверно работает оператор присваивания нового значения в ячейку игрового поля"
+
+game[0, 0] = TicTacToe.COMPUTER_O
+assert game[0, 0] == TicTacToe.COMPUTER_O, "неверно работает оператор присваивания нового значения в ячейку игрового поля"
+
 game.init()
-step_game = 0
-while game:
-    game.show()
+assert game[0, 0] == TicTacToe.FREE_CELL and game[1, 1] == TicTacToe.FREE_CELL, "при инициализации игрового поля все клетки должны принимать значение из атрибута FREE_CELL"
 
-    if step_game % 2 == 0:
-        game.human_go()
-    else:
-        game.computer_go()
-
-    step_game += 1
-
-
-game.show()
-
-if game.is_human_win:
-    print("Поздравляем! Вы победили!")
-elif game.is_computer_win:
-    print("Все получится, со временем")
+try:
+    game[3, 0] = 4
+except IndexError:
+    assert True
 else:
-    print("Ничья.")
+    assert False, "не сгенерировалось исключение IndexError"
 
-# t = TicTacToe()
+game.init()
+print(game.is_human_win)
+# assert game.is_human_win == False and game.is_computer_win == False and game.is_draw == False, "при инициализации игры атрибуты is_human_win, is_computer_win, is_draw должны быть равны False, возможно не пересчитывается статус игры при вызове метода init()"
 #
-# while True:
-#     t.show()
-#     t.human_go()
-#     t.show()
-#     t.computer_go()
+# game[0, 0] = TicTacToe.HUMAN_X
+# game[1, 1] = TicTacToe.HUMAN_X
+# game[2, 2] = TicTacToe.HUMAN_X
+# assert game.is_human_win and game.is_computer_win == False and game.is_draw == False, "некорректно пересчитываются атрибуты is_human_win, is_computer_win, is_draw. Возможно не пересчитывается статус игры в момент присвоения новых значения по индексам: game[i, j] = value"
+#
+# game.init()
+# game[0, 0] = TicTacToe.COMPUTER_O
+# game[1, 0] = TicTacToe.COMPUTER_O
+# game[2, 0] = TicTacToe.COMPUTER_O
+# assert game.is_human_win == False and game.is_computer_win and game.is_draw == False, "некорректно пересчитываются атрибуты is_human_win, is_computer_win, is_draw. Возможно не пересчитывается статус игры в момент присвоения новых значения по индексам: game[i, j] = value"
+
